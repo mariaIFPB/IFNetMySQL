79259ec1b3c82b020bc12bb4c7abdf1a93d261c4
yohannatethysy <thetysy@gmail.com>
2017-12-06 18:48:20 -0300

    Update Usuario.py

--- IFNetMySQL/model/Usuario.py
+++ IFNetMySQL/model/Usuario.py
@@ -3,40 +3,9 @@ from database.configDB import config
 
 class Usuario():
     def __init__(self, nome, email, senha):
-
         self.nome = nome
         self.email = email
         self.senha = senha
-        
-    def inserirUsuario(self, nome, email, senha):
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO TB_Usuario VALUES(?,?, ?)", (nome, email, senha))
-        conn.commit()
-        conn.close()
-
-    def listarUsuarios(self):
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor()
-        cursor.execute("SELECT nome FROM TB_Usuario")
-
-        for linha in cursor.fetchall():
-            print(linha)
-
-        conn.commit()
-        conn.close()
-        
-    def  deletarUsuario ( self, email, senha):
-        conn = mysql.connector.connect(**config)
-        cursor = conn.cursor() 
-        senhaC = self.senha
-        se (senha ==  senhaC):
-            cursor.execute ( "" "
-                DELETE FROM tb_usuario 
-                ONDE email =?
-                "" " , (email))
-         conn.commit ()
-         conn.close ()
                             
     def  __str__ (self):
         retorno  " Usuario %s"  % (self.nome)
